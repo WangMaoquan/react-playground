fbb7ef5c66a65ea5b5d2e242f729d48a6578e0b9
decade <1782976211@qq.com>
2024-09-01 11:04:53 +0800

    feat: editor 基本功能

--- src/components/CodeEditor/index.tsx
+++ src/components/CodeEditor/index.tsx
@@ -2,10 +2,21 @@ import Editor from '../Editor';
 import FileNameList from '../FileNameList';
 
 export default function CodeEditor() {
+  const file = {
+    name: 'decade.tsx',
+    value: 'import lodash from "lodash";\n\nconst a = <div>guang</div>',
+    language: 'typescript',
+  };
+
+  function onEditorChange() {
+    // eslint-disable-next-line prefer-rest-params
+    console.log(...arguments);
+  }
+
   return (
     <div className="flex flex-col h-full">
       <FileNameList />
-      <Editor />
+      <Editor file={file} onChange={onEditorChange} />
     </div>
   );
 }
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,3 +1,67 @@
-export default function Editor() {
-  return <div>editor</div>;
+import MonacoEditor, {
+  OnMount,
+  EditorProps as EdProps,
+} from '@monaco-editor/react';
+import { editor } from 'monaco-editor';
+import { createATA } from './ata';
+
+export interface EditorFile {
+  name: string;
+  value: string;
+  language: string;
+}
+
+interface EditorProps {
+  file: EditorFile;
+  onChange?: EdProps['onChange'];
+  options?: editor.IStandaloneEditorConstructionOptions;
+}
+
+export default function Editor(props: EditorProps) {
+  // onMount 也就是编辑器加载完的回调里，设置 ts 的默认 compilerOptions
+  const handleEditorMount: OnMount = (editor, monaco) => {
+    // 注册快捷键 获取所有的快捷键editor.getSupportedActions().map((a) => a.id)
+    editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyJ, () => {
+      editor.getAction('editor.action.formatDocument')?.run();
+    });
+
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      jsx: monaco.languages.typescript.JsxEmit.Preserve,
+      esModuleInterop: true, // 设置 esModuleInterop 会在编译的时候自动加上 default 属性
+    });
+
+    const ata = createATA((code, path) => {
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        code,
+        `file://${path}`,
+      );
+    });
+
+    editor.onDidChangeModelContent(() => {
+      ata(editor.getValue());
+    });
+
+    ata(editor.getValue());
+  };
+
+  return (
+    <MonacoEditor
+      height="100%"
+      path={props.file.name}
+      language={props.file.language}
+      onMount={handleEditorMount}
+      value={props.file.value}
+      options={{
+        fontSize: 14, // 字体大小
+        scrollBeyondLastLine: false, // 取消编辑器右侧的滚动条
+        minimap: {
+          enabled: false,
+        }, // 缩略图
+        scrollbar: {
+          verticalScrollbarSize: 6,
+          horizontalScrollbarSize: 6,
+        }, // 滚动条样式
+      }}
+    />
+  );
 }
