6f03b3b664b2ee948dcfb8690f24c1249435640c
decade <1782976211@qq.com>
2024-09-02 12:17:24 +0800

    style: filenameitem 内容不换行

--- src/components/FileNameItem/index.tsx
+++ src/components/FileNameItem/index.tsx
@@ -13,7 +13,7 @@ export const FileNameItem: React.FC<FileNameItemProps> = (props) => {
 
   return (
     <div
-      className={`inline-flex py-2 px-3 text-[0.8125rem] leading-[1.25rem] cursor-pointer items-center border-b-[0.1875rem] border-transparent ${
+      className={`inline-flex py-2 px-3 text-[0.8125rem] leading-[1.25rem] cursor-pointer items-center border-b-[0.1875rem] border-transparent whitespace-nowrap ${
         actived ? 'text-sky-400 border-sky-400' : ''
       }`}
       onClick={onClick}
